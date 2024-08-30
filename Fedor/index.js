db53d1a93d82c17257c736527bdcfbe8979c15d9
MaxymNest <133804868+MaxymNest@users.noreply.github.com>
2024-08-30 22:58:39 +0300

    Add files via upload

---
+++ Fedor/index.js
@@ -0,0 +1,18 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+export default function (req, res) {
+  const proxy = createProxyMiddleware({
+    target: 'https://drive.google.com',
+    changeOrigin: true,
+    pathRewrite: {
+      '^/proxy': '', // Убираем "/proxy" из пути запроса
+    },
+    onProxyRes(proxyRes) {
+      proxyRes.headers['Access-Control-Allow-Origin'] = '*'; // Добавляем заголовки CORS
+      proxyRes.headers['Access-Control-Allow-Methods'] = 'GET,OPTIONS,PATCH,DELETE,POST,PUT';
+      proxyRes.headers['Access-Control-Allow-Headers'] = 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version';
+    },
+  });
+
+  return proxy(req, res);
+}
